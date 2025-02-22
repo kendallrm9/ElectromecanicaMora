d96b2691f4ab06afa668b3fc8b6e1047c75ff1bb
Kendall Reyes <kendallrm9@gmail.com>
2025-02-21 18:22:30 -0600

    cambios varios

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+  const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("superponer");
+  sidebar.classList.toggle("show");
+  overlay.classList.toggle("active");
+}
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const overlay = document.querySelector(".superponer");
+  const body = document.body;
+
+  const observer = new MutationObserver(() => {
+    if (overlay.classList.contains("active")) {
+      body.style.overflowY = "hidden";
+    } else {
+      body.style.overflowY = "auto"; // Restaura el scroll cuando se cierra
+    }
+  });
+
+  observer.observe(overlay, { attributes: true, attributeFilter: ["class"] });
+});
